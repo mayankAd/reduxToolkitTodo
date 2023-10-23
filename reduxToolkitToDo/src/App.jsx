c44e73e897cc6c9cd982dab86fa9828a45e3f874
mayankAd <blockrookie@gmail.com>
2023-10-23 23:23:05 +0530

    commit

--- reduxToolkitToDo/src/App.jsx
+++ reduxToolkitToDo/src/App.jsx
@@ -2,13 +2,16 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import AddTodos from './components/AddTodos'
+import ToDos from './components/ToDos'
 
 function App() {
-  const [count, setCount] = useState(0)
-
+  
   return (
     <>
-      <h1>Learn about redux Toolkit</h1>
+      <h1>Learn about redux toolkit</h1>
+      <AddTodos />
+      <ToDos />
     </>
   )
 }
